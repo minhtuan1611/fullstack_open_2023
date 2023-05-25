6ae70ff2623dda5e95f41d66a181690aca5cd940
minhtuan1611 <tuan.mn51@gmail.com>
2023-05-25 18:03:17 +0300

    add const

--- part2/hehee/src/App.js
+++ part2/hehee/src/App.js
@@ -1,12 +1,14 @@
-const App = (props) => {
-  const { notes } = props;
+const Note = ({ note }) => {
+  return <li> {note.content}</li>;
+};
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <li key={note.id}>{note.content}</li>
+          <Note key={note.id} note={note} />
         ))}
       </ul>
     </div>

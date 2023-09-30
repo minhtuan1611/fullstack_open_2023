dd93f99adbada6cc5b26b586f3d71d2664d3884b
minhtuan1611 <tuan.mn51@gmail.com>
2023-09-30 11:21:56 +0300

    Add: complete 6c

--- Part6/redux-anecdotes-main/src/components/Notification.js
+++ Part6/redux-anecdotes-main/src/components/Notification.js
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux'
-
-// const Notification = () => {
-//   const notification = useSelector((state) => state.notification)
-
-//   if (!notification) {
-//     return null
-//   }
-
-//   const style = {
-//     border: 'solid',
-//     padding: 10,
-//     borderWidth: 1,
-//   }
-
-//   return <div style={style}>{notification}</div>
-// }
-
-// export default Notification
-
 import React from 'react'
 import { useSelector } from 'react-redux'
 

462bdd7978dde99f4b990ac233d57247cd7a4923
minhtuan1611 <tuan.mn51@gmail.com>
2023-10-23 17:16:48 +0300

    Irila's diary initial

--- Part 9/Irila/backend/src/index.ts
+++ Part 9/Irila/backend/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
-
-import diaryRouter from "./routes/diaries";
 const app = express();
+import diaryRouter from "./routes/diaries";
 app.use(express.json());
 
 const PORT = 3000;
